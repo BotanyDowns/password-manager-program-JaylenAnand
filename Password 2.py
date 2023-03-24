c8192957dce89d17c7262109c064448b0ec66bc3
JaylenAnand <126726097+JaylenAnand@users.noreply.github.com>
2023-03-24 13:12:18 +1300

    Add files via upload

---
+++ Password 2.py
@@ -0,0 +1,43 @@
+#Version 2: Password Manager with Improved Functionality
+
+#creating an empty dictionary to store account name and password
+passwords = {}
+
+#using 3 functions like adding passwords to account and asking to get password from dictionary
+def add_password(account, password):
+    #Check if account name already exists in the dictionary
+    if account in passwords:
+        print(f"Account name '{account}' already exists.")
+    else:
+        passwords[account] = password
+        print(f"Password added for account '{account}'.")
+
+def get_password(account):
+    password = passwords.get(account, None)
+    if password:
+        print(f"Password for account '{account}': {password}")
+    else:
+        print(f"No such account name found '{account}'.")
+
+def main():
+    while True:
+        print("1. Add Password")
+        print("2. Get Password")
+        print("3. Exit")
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == "1":
+            account = input("Enter account name: ")
+            password = input("Enter password: ")
+            add_password(account, password)
+        elif choice == "2":
+            account = input("Enter account name: ")
+            get_password(account)
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice")
+
+if __name__ == "__main__":
+    main()
