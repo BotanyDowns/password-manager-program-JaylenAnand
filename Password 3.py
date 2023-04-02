e0493bbaa7974100b6504a7525e4927dbfe834e8
JaylenAnand <126726097+JaylenAnand@users.noreply.github.com>
2023-04-02 15:32:54 +1200

    Update Password 3.py

--- Password 3.py
+++ Password 3.py
@@ -1,5 +1,4 @@
 #Version 3: GUI Password Manager
-
 import tkinter as tk
 import json
 
@@ -8,29 +7,50 @@ class PasswordManager:
         self.master = master
         master.title("Password Manager")
 
-        #Create account label and entry widget
-        self.account_label = tk.Label(master, text="Account:")
-        self.account_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
-        self.account_entry = tk.Entry(master)
-        self.account_entry.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+        #Create login frame
+        self.login_frame = tk.Frame(master)
+        self.login_frame.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
 
-        #Create password label and entry widget
-        self.password_label = tk.Label(master, text="Password:")
-        self.password_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
-        self.password_entry = tk.Entry(master, show="*")
-        self.password_entry.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+        #Add login icon to top center of login frame
+        self.login_icon = tk.PhotoImage(file="Login Icon.png")
+        self.login_icon_label = tk.Label(self.login_frame, image=self.login_icon)
+        self.login_icon_label.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky=tk.N)
 
-        #Create add password button
-        self.add_button = tk.Button(master, text="Add Password", command=self.add_password)
+        self.login_account_label = tk.Label(self.login_frame, text="Account:")
+        self.login_account_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        self.login_account_entry = tk.Entry(self.login_frame)
+        self.login_account_entry.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+
+        self.login_password_label = tk.Label(self.login_frame, text="Password:")
+        self.login_password_label.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
+        self.login_password_entry = tk.Entry(self.login_frame, show="*")
+        self.login_password_entry.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W)
+
+        self.login_button = tk.Button(self.login_frame, text="Login", command=self.login)
+        self.login_button.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky=tk.W)
+
+        self.login_message_label = tk.Label(self.login_frame, text="")
+        self.login_message_label.grid(row=4, column=0, columnspan=2, padx=5, pady=5, sticky=tk.W)
+
+        #Create account frame
+        self.account_frame = tk.Frame(master)
+        self.account_frame.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+        self.account_frame.grid_remove()
+
+        self.account_title_label = tk.Label(self.account_frame, text="Account")
+        self.account_title_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+
+        self.password_title_label = tk.Label(self.account_frame, text="Password")
+        self.password_title_label.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+
+        self.add_button = tk.Button(self.account_frame, text="Add Password", command=self.add_password)
         self.add_button.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
 
-        #Create get password button
-        self.get_button = tk.Button(master, text="Get Password", command=self.get_password)
+        self.get_button = tk.Button(self.account_frame, text="Get Password", command=self.get_password)
         self.get_button.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W)
 
-        #Create message label
-        self.message_label = tk.Label(master, text="")
-        self.message_label.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky=tk.W)
+        self.account_message_label = tk.Label(self.account_frame, text="")
+        self.account_message_label.grid(row=3, column=0, columnspan=2, padx=5, pady=5, sticky=tk.W)
 
         #Load passwords from file
         try:
@@ -40,36 +60,36 @@ class PasswordManager:
         except FileNotFoundError:
             passwords = {}
 
+    def login(self):
+        account = self.login_account_entry.get()
+        password = self.login_password_entry.get()
+        
+        if account == "" or password == "":
+            self.login_message_label.config(text="Please enter both account and password.")
+        else:
+            self.login_frame.grid_remove()
+            self.account_frame.grid()
+   
+        
     def add_password(self):
-        account = self.account_entry.get()
-        password = self.password_entry.get()
-
-        if account == "":
-            self.message_label.config(text="Please enter an account name.")
-        elif account in passwords:
-            self.message_label.config(text=f"Account name '{account}' already exists.")
-        else:
-            passwords[account] = password
-            with open('passwords.json', 'w') as f:
-                json.dump(passwords, f)
-            self.message_label.config(text=f"Password added for account '{account}'.")
-            self.account_entry.delete(0, tk.END)
-            self.password_entry.delete(0, tk.END)
-
+        account = input("Enter account: ")
+        password = input("Enter password: ")
+        passwords[account] = password
+        self.save_passwords()
+        self.account_message_label.config(text="Password added successfully.")
+    
     def get_password(self):
-        account = self.account_entry.get()
-
-        if account == "":
-            self.message_label.config(text="Please enter an account name.")
-        elif account not in passwords:
-            self.message_label.config(text=f"No password found for account '{account}'.")
-        else:
+        account = input("Enter account: ")
+        if account in passwords:
             password = passwords[account]
-            self.message_label.config(text=f"Password for account '{account}': {password}")
-            self.account_entry.delete(0, tk.END)
-            self.password_entry.delete(0, tk.END)
+            self.account_message_label.config(text=f"Password for {account}: {password}")
+        else:
+            self.account_message_label.config(text=f"No password found for {account}.")
+    
+    def save_passwords(self):
+        with open('passwords.json', 'w') as f:
+            json.dump(passwords, f)
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    password_manager = PasswordManager(root)
-    root.mainloop()
+root = tk.Tk()
+password_manager = PasswordManager(root)
+root.mainloop()
